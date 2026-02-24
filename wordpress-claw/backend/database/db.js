3c98d5a578c5b27dcdcba4803567211aa699e7fc
xBalzerian <balzgaming77@gmail.com>
2026-02-24 12:27:25 +0800

    Fix: Remove circular dependency in database migrations

--- wordpress-claw/backend/database/db.js
+++ wordpress-claw/backend/database/db.js
@@ -35,6 +35,6 @@ console.log('Database initialized at:', DB_PATH);
 
 // Run migrations
 const { runMigrations } = require('./migrations');
-runMigrations();
+runMigrations(db);
 
 module.exports = db;
--- wordpress-claw/backend/database/migrations.js
+++ wordpress-claw/backend/database/migrations.js
@@ -1,6 +1,4 @@
-const db = require('./db');
-
-function runMigrations() {
+function runMigrations(db) {
     console.log('Running database migrations...');
     
     try {
